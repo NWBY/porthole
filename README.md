3cd060d1fc306b7f852de23b92132bda6cc76af2
Sam Newby <sam.newby19@gmail.com>
2024-08-24 23:17:10 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# porthole
+
+A simple docker container monitoring tool.
+
+To see porthole in action you can run it on your local machine (make sure you already have some containers running):
+
+```bash
+cargo run
+```
---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use bollard::container::{ListContainersOptions, StatsOptions};
+use bollard::Docker;
+
+use futures_util::stream::StreamExt;
+
+use std::collections::HashMap;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let docker = Docker::connect_with_socket_defaults().unwrap();
+
+    let mut filter = HashMap::new();
+    filter.insert(String::from("status"), vec![String::from("running")]);
+    let containers = &docker
+        .list_containers(Some(ListContainersOptions {
+            all: true,
+            filters: filter,
+            ..Default::default()
+        }))
+        .await?;
+
+    if containers.is_empty() {
+        panic!("No running containers");
+    } else {
+        for container in containers {
+            let container_id = container.id.as_ref().unwrap();
+            let stream = &mut docker
+                .stats(
+                    container_id,
+                    Some(StatsOptions {
+                        stream: false,
+                        ..Default::default()
+                    }),
+                )
+                .take(1);
+
+            while let Some(Ok(stats)) = stream.next().await {
+                println!(
+                    "{} - {:?}: {:?} {:?}",
+                    container_id, &container.names, container.image, stats
+                );
+            }
+        }
+    }
+
+    Ok(())
+}
